d3c9e6bb58c135bc1c246de69bea53318dc36199
HoangQuanGITHUB <103574748+HoangQuanGITHUB@users.noreply.github.com>
2022-05-04 21:42:24 +0700

    Add files via upload

---
+++ README.rst
@@ -0,0 +1,9 @@
+Hello!
+Welcome to web2exe made by HoangQuanMC!
+To install, you need "pip". If you don't have "pip" you should insall "pip"
+Btw, to install type : "pip install web2exe"
+Thanks
+
+Req module : 
+PyQt5
+Pyinstaller
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='html2exe_python',
+    version='0.6',
+    license='MIT',
+    author="HoangQuanGITHUB",
+    author_email='caohoangquannguyen.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/HoangQuanMCGITHUB/web2exe_python',
+    keywords='web to app',
+    install_requires=[
+          'PyQt5',
+          'os',
+          'sys',
+          'pyinstaller'
+      ],
+)
